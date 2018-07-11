d1fd1fad93681a686fb9c9faebef5bbfb022de2a
milena-mica <trajkovic.milena.mica@gmail.com>
2018-07-11 16:52:32 +0200

    Updating the Percentages Budget Ctrl

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ var budgetController = (function() {
      this.id = id;
      this.description = description;
      this.value = value;
+     this.percentage = -1;
+   };
+
+   Expense.prototype.calcPercentage = function(totalIncome) {
+      if (totalIncome > 0) {
+      this.percentage = Math.round((this.value / totalIncome) * 100);
+      } else {
+        this.percentage = -1;
+      }
+   };
+
+   Expense.prototype.getPercentage = function() {
+     return this.percentage;
    };
 
    var Income = function(id, description,value) {
@@ -104,8 +117,37 @@ var budgetController = (function() {
         // Expense = 100 and income 200, spent 50% = 100/200 = 0.5 * 100
     },
 
+    calculatePercentages: function()  {
+        
+      /*
+      a=20
+      b=10
+      c=40
+      income = 100
+      a=20/100=20%
+      b=10/100=10%
+      c=40/100=40%
+      */
+
+      data.allItems.exp.forEach(function(cur) {
+        cur.calcPercentage(data.totals.inc);
+      });
+    },
+
+    getPercentages: function() {
+        var allPerc = data.allItems.exp.map(function(cur) {
+          return cur.getPercentage();
+        });
+        return allPerc;
+    },
+
     getBudget: function() {
-       return
+       return {
+          budget: data.budget,
+          totalInc: data.totals.inc,
+          totalExp: data.totals.exp,
+          percentage: data.percentage
+       };
     },
 
     testing: function() {
@@ -246,10 +288,13 @@ var controller = (function(budgetCtrl, UICtrl) {
    var updatePercentages = function() {
       
       // 1. Calculate percentages
+      budgetCtrl.calculatePercentages();
 
       // 2. Read percentages from the budget controller
+     var percentages = budgetCtrl.getPercentages();
 
       // 3. Update the UI with the new percentages
+      console.log(percentages);
    };
 
 

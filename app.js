faf45237080d2ef5c79c73ed657b3bdf2d65c38c
milena-mica <trajkovic.milena.mica@gmail.com>
2018-07-19 18:29:54 +0200

    Improving the UX

--- app.js
+++ app.js
@@ -205,6 +205,11 @@ var UIController = (function() {
 
   };
 
+  var nodeListForEach = function(list, callback) {
+    for (var i = 0; i < list.length; i++) {
+         callback(list[i], i);
+    }
+};
 
   return {
     getinput: function() {
@@ -282,14 +287,6 @@ var UIController = (function() {
 
        var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-
-       var nodeListForEach = function(list, callback) {
-           for (var i = 0; i < list.length; i++) {
-                callback(list[i], i);
-           }
-       };
-
-
        nodeListForEach(fields, function(current, index) {
           
           if (percentages[index] > 0) {
@@ -312,6 +309,21 @@ var UIController = (function() {
 
          year = now.getFullYear(); 
          document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+     },
+
+     changedType: function() {
+         
+         var fields = document.querySelectorAll(
+            DOMstrings.inputType + ',' +
+            DOMstrings.inputDescription + ',' +
+            DOMstrings.inputValue);
+         
+
+         nodeListForEach(fields, function(cur) {
+            cur.classList.toggle('red-focus');
+         });
+
+         document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
      },
 
      
@@ -339,6 +351,8 @@ var controller = (function(budgetCtrl, UICtrl) {
      });
 
      document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+     document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 
    };
 
